6b44ff81efafa9a52f262288c473d77952eeaa4d
Adonis255 <adamsomondi05@gmail.com>
2026-07-07 04:49:24 +0300

    Fix vercel.json configuration

--- api/index.py
+++ api/index.py
@@ -2,7 +2,6 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from app import app  # your Flask app instance
+from app import app
 
-# Vercel needs this
 app = app
